3a8d1a3877d027c9acb0de4eb52574d617de2837
AndrewJeannett <andrewjeannett@gmail.com>
2018-02-23 18:06:51 -0600

    sound!!

--- Assets/LightArray.cs
+++ Assets/LightArray.cs
@@ -1,39 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using static UnityStandardAssets.Utility.TimedObjectActivator;
 
 public class LightArray : MonoBehaviour {
-    public List <GameObject> lightsOff= new List<GameObject>();
-    public List <GameObject> lightsOn = new List<GameObject>();
+    public List<GameObject> lightsOff = new List<GameObject>();
+    public List<GameObject> lightsOn = new List<GameObject>();
     public int lightPos;
     public int trail;
     private bool stop = false;
-	// Use this for initialization
-	
-	// Update is called once per frame
-	void Update () {
+    public AudioSource winSound;
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update() {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             stop = true;
-        }else if (Input.GetKeyDown(KeyCode.DownArrow))
+          
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             stop = false;
         }
-        
+
         if (lightsOn.Count < 1)
         {
             for (int i = 0; i < trail; i++)
             {
-                lightsOn.Add(lightsOff[i+lightPos]);
-                lightsOff.Remove(lightsOff[i+lightPos]);
-                
+                lightsOn.Add(lightsOff[i + lightPos]);
+                lightsOff.Remove(lightsOff[i + lightPos]);
+
             }
         }
-       Invoke("lightClear", .0001f);
+      
+
+
+        Invoke("lightClear", .0001f);
+
         for (int i = 0; i < lightsOn.Count; i++)
         {
             lightsOn[i].SetActive(true);
         }
+
         for (int i = 0; i < lightsOff.Count; i++)
         {
             lightsOff[i].SetActive(false);
@@ -44,22 +52,34 @@ public class LightArray : MonoBehaviour {
 
         // }
         // lightList.Remove(Random.Range(0, 90));
-    }
-    void lightClear()
-    {
-       if (stop == false)
-       {
 
-            for (int i = 0; i < 100; i++)
-            {
-                lightsOff.Add(lightsOn[i]);
-                lightsOn.Remove(lightsOn[i]);
 
-            }
-
-            lightsOn.Clear();
+        if ((lightsOn[0].tag == "winLight") && (stop == true))
+        {
+            winSound.Play();
+            Debug.Log("WINNER");
+            stop = false;
+            trail = 40;
         }
     }
+
+
+
+        void lightClear()
+    {
+            if (stop == false)
+            {
+
+                for (int i = 0; i < 100; i++)
+                {
+                    lightsOff.Add(lightsOn[i]);
+                    lightsOn.Remove(lightsOn[i]);
+
+                }
+
+                lightsOn.Clear();
+            }
+        }
 
     }
 
